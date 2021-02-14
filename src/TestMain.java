53e401e8af8b0ac3aa2dfe57ceae576118217562
nam2626 <nam2626@gmail.com>
2021-02-14 22:21:47 +0900

    1019 커밋

--- src/TestMain.java
+++ src/TestMain.java
@@ -5,6 +5,7 @@ public class TestMain {
 		System.out.println("main branch");
 		System.out.println("member1 branch");
 		System.out.println("main branch-today"); 
+		System.out.println("main 1019");
 	}
 
 }
